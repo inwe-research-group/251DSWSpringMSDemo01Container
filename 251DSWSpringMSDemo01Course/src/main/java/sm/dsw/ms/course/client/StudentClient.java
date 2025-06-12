361c734370e81a640ab5def6be44fa4c3358c1fa
inwe-research-group <zmamanir@unmsm.edu.pe>
2025-06-11 20:25:56 -0500

    add anotacion to application api courses

--- 251DSWSpringMSDemo01Course/src/main/java/sm/dsw/ms/course/client/StudentClient.java
+++ 251DSWSpringMSDemo01Course/src/main/java/sm/dsw/ms/course/client/StudentClient.java
@@ -7,7 +7,7 @@ import sm.dsw.ms.course.dto.StudentDTO;
 
 import java.util.List;
 
-@FeignClient(name="251DSWSpringMSDemo01Student",url="localhost:8091/api/student")
+@FeignClient(name="251DSWSpringMSDemo01Student",url="localhost:8091")
 public interface StudentClient {
     @GetMapping("/api/student/search-by-course/{courseId}")
     List<StudentDTO> findAllStudentByCourse(@PathVariable Long courseId);
